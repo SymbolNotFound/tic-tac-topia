be96112074a820f9b187d2fad1483295ded98832
Kevin Damm <github@kevindamm.com>
2024-01-20 21:10:11 -0500

    Initial version of a quasar SPA for Tic-Tac-Meta
    This is the frontend and client for the demo application
    showing the generality possible with GGDL and a General Game Playing
    service.

---
+++ webapp/src/layouts/TicTacMeta.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Tic-Tac-Meta
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+
+const essentialLinks: EssentialLinkProps[] = [
+  {
+    title: 'About',
+    caption: 'Tic-Tac-Meta docs',
+    icon: 'school',
+    link: 'https://ggp-demo.symbolnotfound.com/docs',
+  },
+  {
+    title: 'GitHub',
+    caption: 'github: SymbolNotFound/ggp-demo',
+    icon: 'code',
+    link: 'https://github.com/SymbolNotFound/ggp-demo'
+  },
+];
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
