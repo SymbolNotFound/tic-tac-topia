ae36091f23cac7bf0385fb29730d38d5bb5ce6ef
Kevin Damm <github@kevindamm.com>
2025-05-21 23:04:22 -0700

    enable Nuxt 4 compatibility
    might as well open it up, there's nothing dragging us back as far as I can tell
    
    doesn't seem to fix the auto-import scanning for types into Vue SFCs, though.  no matter, for my own types I'd rather be explicit about importing them anyway

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  future: {
+    compatibilityVersion: 4
+  },
+
   // Root CSS where dependencies are imported into layers, making cascading explicit.
   css: ['~/assets/css/app.css'],
 
