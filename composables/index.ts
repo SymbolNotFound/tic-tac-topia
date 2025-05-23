b05818a33fde0391e1e13798bfd9a19ecfd7c43b
Kevin Damm <github@kevindamm.com>
2025-05-22 23:34:27 -0800

    some basic equipment types used by different surfaces

--- composables/index.ts
+++ composables/index.ts
@@ -1,4 +1,28 @@
 
 
-export type Marker = string
-export const BLANK: Marker = " "
+export type EMPTY = " "
+export type Equipment = EMPTY | Marker | D6 | Unit | NumberTile | PlayingCard
+
+export type Marker = " " | "X" | "O"
+
+export type D6 = 1 | 2 | 3 | 4 | 5 | 6;
+
+export type Unit = { color: "RED" | "BLUE" }
+
+export type NumberTile = {
+  front: number
+  back: number
+}
+
+export type PlayingCard = {
+  suit: "HEARTS" | "SPADES" | "DIAMONDS" | "CLUBS"
+  rank: "ACE" | "2" | "3" | "4" | 
+      "5" | "6" | "7" | "8" | "9" |
+      "10" | "JACK" | "QUEEN" | "KING"
+}
+
+export type SetCard = {
+  count: 1 | 2 | 3
+  color: "RED" | "GREEN" | "PURPLE"
+  shape: "ROUND" | "POINTY" | "WIGGLE"
+}
